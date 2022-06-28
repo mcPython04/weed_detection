c69d95ffb5c18244aa4612eecbf8cbf64ddec039
mcPython04 <chen1911893611@gmail.com>
2022-06-28 13:23:44 -0500

    added GUI file

---
+++ gui_app.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("1920x1080") # window size
+window.title("Weed Detection") # window name
+
+# widget code will go here
+
+# button
+start = Button(window, text='Start')
+start.pack()
+
+window.mainloop()
